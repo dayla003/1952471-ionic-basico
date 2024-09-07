748cd271501c38df3e5d1d762a3170ad79616794
Dayla Carrizales <134174974+dayla003@users.noreply.github.com>
2024-09-07 16:42:05 +0000

    Practica 4

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-var userTxt = document.getElementById("userTxt");
-var passwordTxt = document.getElementById("passwordTxt");
+var articulo = document.getElementById("articulo");
+var monto = document.getElementById("monto");
+
 var parrafo = document.querySelector("p");
+var lista = document.querySelector("ul");
+var total = 0;
 
-function iniciarSesion() {
-  var userValue = userTxt.value;
-  parrafo.innerHTML = `Log in correcto <strong>${userValue}</strong>`;
-}
-
-function registro() {
-  var userValue = userTxt.value;
-  parrafo.innerHTML = `Registro correcto <strong>${userValue}</strong>`;
+function calcularTotal() {
+  var articuloValor = articulo.value;
+  var montoValor = monto.value;
+  total = total + parseInt(montoValor);
+  var elemento = document.createElement("li");
+  elemento.innerHTML = `${articuloValor}_________<strong>$${montoValor}</strong>`;
+  lista.appendChild(elemento);
+  parrafo.innerHTML = `<strong>Total_________$${total}</strong>`;
+  monto.value = "";
+  articulo.value = "";
 }
