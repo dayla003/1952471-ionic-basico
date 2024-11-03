6ac6e110787c8f15167cff6143ede0838ba3e8c1
dayla003 <dayla.carrizalesortg@uanl.edu.mx>
2024-11-03 15:24:24 -0600

    Practica 13: Build de App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,34 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
 import { IonicModule } from '@ionic/angular';
 import { ListaAlumnosComponent } from './lista-alumnos/lista-alumnos.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp,IonicModule, ListaAlumnosComponent],
+  imports: [IonApp, IonicModule, ListaAlumnosComponent, CommonModule],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(public platform: Platform) {
+    this.initializeApp();
+  }
 
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.checkPlatform();
+    });
+  }
+
+  checkPlatform() {
+    if (this.platform.is('android')) {
+      console.log('La app se está ejecutando en Android');
+    } else if (this.platform.is('ios')) {
+      console.log('La app se está ejecutando en iOS');
+    } else if (this.platform.is('desktop')) {
+      console.log('La app se está ejecutando en un navegador web');
+    }
+  }
 }
